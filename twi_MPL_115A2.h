2887ef5e5ce76e302223e8aa28eaf4f9684e4435
Ismael Khalique <ismael.khalique@gmail.com>
2018-07-24 20:16:55 -0400

    Update twi_MPL_115A2.h

--- twi_MPL_115A2.h
+++ twi_MPL_115A2.h
@@ -10,17 +10,3 @@
 
 
 
-/**< pin configuration first */
-//bool twi_pin_config_pressure_sensor_init();
-//void twi_set_sda_high(uint8_t scl);
-//void twi_set_sda_high(uint8_t sda);
-
-///**< not defined yet. Talk to Ismael so maybe he can define the rest of the functions */
-//// note: they don'thave to be voids and set arguments
-//void twi_start_condition();
-//void twi_stop_condition();
-//void twi_send_ack();
-//void twi_send_nack();
-//void twi_set_as_input(uint8_t sda); //maybe idk!
-//uint16_t twi_read_data(); // maybe idk!
-
